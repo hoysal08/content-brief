aaa3766cda2067a5de7a86cc9af330a544a7e463
Sooraj Hoysal <surajhoysal817@gmail.com>
2024-01-14 22:48:29 +0530

    refactored user input validation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import { MAX_INPUT_SIZE, rand } from "~/constants";
 const Hero = () => {
   const [inputText, setInputText] = useState<InputText>("");
   const [inputCount, setInputCount] = useState<number>(0);
-  const [showSummary, setShowSummary] = useState<boolean>(true);
+  const [validInput, setvalidInput] = useState<boolean>(true);
   const [outputText, setOutputText] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -13,9 +13,9 @@ const Hero = () => {
   useEffect(() => {
     setInputCount(inputText.length);
     if (inputText.length > MAX_INPUT_SIZE || inputText.length === 0) {
-      setShowSummary(false);
+      setvalidInput(false);
     } else {
-      setShowSummary(true);
+      setvalidInput(true);
     }
   }, [inputText]);
 
@@ -39,7 +39,7 @@ const Hero = () => {
           Paste the text here to summarise it:
         </h3>
         <p
-          className={`${showSummary ? "" : "text-red-600"}`}
+          className={`${validInput ? "" : "text-red-600"}`}
         >{`${inputCount}/${MAX_INPUT_SIZE}`}</p>
       </div>
       <div className="h-[40vh] w-full ">
@@ -51,7 +51,7 @@ const Hero = () => {
       <div className="item  m-3 flex w-full items-end justify-end text-lg ">
         <button
           className={`w-full rounded bg-[#0092ca] p-2 disabled:opacity-75 lg:w-max `}
-          disabled={!showSummary}
+          disabled={!validInput || loading}
           onClick={handleSummarize}
         >
           Summarize
